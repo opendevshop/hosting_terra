5c532dcb7622aa8a8516755cb4c25d73d1cebba1
Jon Pugh <jon@thinkdrop.net>
2016-05-21 01:14:15 -0400

    Moving app and environment add to the db service.

--- drush/Provision/Service/db/terra.php
+++ drush/Provision/Service/db/terra.php
@@ -17,23 +17,38 @@ class Provision_Service_db_terra extends Provision_Service_db {
   }
 
   /**
+   * Verifies database connection and commands
+   */
+   function create_site_database($creds = array()) {
+     $project = d()->project;
+     $environment = d()->environment;
+
+     $project_object =  d("@project_{$project}");
+     $environment_object = (object) $project_object->project['environments'][$environment];
+
+     $output = d()->db_server->service('Process')->process("terra status {$project}", '', "Install");
+     if (strpos($output, $project_object->project->git_url) === FALSE) {
+      $cmd = "terra app:add --description='Added by DevShop' {$project} {$project_object->project->git_url} ";
+      d()->db_server->service('Process')->process($cmd, '', "Install");
+    }
+
+     // 2. Add environment.
+     $environment_object = (object) d("@project_{$project}")->project['environments'][$environment];
+     $cmd = "terra environment:add {$project} {$environment} {$environment_object->repo_root}";
+     d()->db_server->service('Process')->process($cmd, '', "Terra");
+
+     // 3. enable environment.
+     $cmd = "terra environment:enable {$project_object->project->name} {$environment_object->name}";
+    d()->db_server->service('Process')->process($cmd, '', "Terra");
+
+  }
+
+  /**
    * Sync filesystem changes to the server hosting this service.
    */
   function sync($path = NULL, $additional_options = array()) {
     drush_log('Provision_Service_db_terra::sync()', 'ok');
 //    return $this->server->sync($path, $additional_options);
-  }
-  /**
-   * This method is at the core of preparing the site.
-   *
-   * Use this method to trigger the creation of the Rancher environments.
-   *
-   * @param array $creds
-   * @return bool
-   */
-  function create_site_database($creds = array()) {
-    drush_log('Provision_Service_db_terra::create_site_database()', 'ok');
-    return TRUE;
   }
 
   function can_create_database() {
